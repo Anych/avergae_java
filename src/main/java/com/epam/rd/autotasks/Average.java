f6dad5b92983f9e611aa053e1ca78175a29fcfaa
Anych2 <anuar123@mail.ru>
2022-08-09 07:35:02 +0000

    Check my answer

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
-
+        String input = scanner.nextLine();
+        
+        String[] ints = input.split(" ");
+        int sum = 0;
+        
+        for (String anInt : ints) {
+            int num = Integer.parseInt(anInt);
+            sum += num;
+        }
+        
+        int result = sum / (ints.length - 1);
+        System.out.println(result);
     }
 
 }
